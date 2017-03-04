a126d9e7c0779072df42008351b2f002badd23c3
Raczkowski <raczkowskir@yahoo.com>
2017-03-04 13:12:26 +0100

    First activity - draft, added: - voice - image

--- app/src/main/java/com/yahoo/raczkowskir/thestory/MainActivity.java
+++ app/src/main/java/com/yahoo/raczkowskir/thestory/MainActivity.java
@@ -1,13 +1,12 @@
 package com.yahoo.raczkowskir.thestory;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        final ImageButton B1 = (ImageButton) findViewById(R.id.b1);
+        B1.setOnClickListener(new View.OnClickListener() {
+    @Override
+            public void onClick (View view){
+        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.rr1);
+        mediaPlayer.start(); // no need to call prepare(); create() does that for you
+    }
+        });
+
+
+    }
+      /*  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -26,10 +37,11 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "I will change this.", Snackbar.LENGTH_LONG)
                         .setAction("RR", null).show();
             }
-        });
-    }
+        });*/
+
 
     @Override
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
